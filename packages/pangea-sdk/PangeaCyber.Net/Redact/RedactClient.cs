236f148fd93ed8eaacfab53794c3fb9bd23c6f95
David Wayman <david.wayman@pangea.cloud>
2023-07-25 13:22:06 -0700

    Support operationid doc tag, add embargo doc strings

--- packages/pangea-sdk/PangeaCyber.Net/Redact/RedactClient.cs
+++ packages/pangea-sdk/PangeaCyber.Net/Redact/RedactClient.cs
@@ -30,10 +30,9 @@ namespace PangeaCyber.Net.Redact
         }
 
         /// <kind>method</kind>
-        /// <summary>
-        /// Redact sensitive information from provided text.
-        /// </summary>
-        /// <remarks>Redact sensitive information from provided text.</remarks>
+        /// <summary>Redact sensitive information from provided text.</summary>
+        /// <remarks>Redact</remarks>
+        /// <operationid>redact_post_v1_redact</operationid>
         /// <param name="request" type="PangeaCyber.Net.Redact.RedactTextRequest">RedactRequest with text and optional parameters</param>
         /// <returns>Response&lt;RedactTextResult&gt;</returns>
         /// <exception cref="PangeaException"></exception>
@@ -42,7 +41,6 @@ namespace PangeaCyber.Net.Redact
         /// <code>
         /// var request = new RedactTextRequest.RequestTextBuilder("Jenny Jenny... 415-867-5309").Build();
         /// var response = await client.RedactText(request);
-        ///
         /// </code>
         /// </example>
         public async Task<Response<RedactTextResult>> RedactText(RedactTextRequest request)
@@ -51,10 +49,9 @@ namespace PangeaCyber.Net.Redact
         }
 
         /// <kind>method</kind>
-        /// <summary>
-        /// Redact sensitive information from provided structured data.
-        /// </summary>
-        /// <remarks>Redact sensitive information from structured data (e.g., JSON).</remarks>
+        /// <summary>Redact sensitive information from structured data (e.g., JSON).</summary>
+        /// <remarks>Redact structured</remarks>
+        /// <operationid>redact_post_v1_redact_structured</operationid>
         /// <param name="request" type="PangeaCyber.Net.Redact.RedactTextRequest">RedactRequest with structured data</param>
         /// <returns>Response&lt;RedactStructuredResult&gt;</returns>
         /// <exception cref="PangeaException"></exception>
@@ -63,7 +60,6 @@ namespace PangeaCyber.Net.Redact
         /// <code>
         /// var request = new RedactStructuredRequest.RequestStructureBuilder("Jenny Jenny... 415-867-5309").Build();
         /// var response = await client.RedactStructured(request);
-        ///
         /// </code>
         /// </example>
         public async Task<Response<RedactStructuredResult>> RedactStructured(RedactStructuredRequest request)
