6dc0fffc01b8ef25b3f4bc3f5044db1839920364
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-07-27 10:43:01 -0300

    user password breached example

---
+++ examples/Intel/User/PasswordBreached/Program.cs
@@ -0,0 +1,73 @@
+﻿using PangeaCyber.Net;
+using PangeaCyber.Net.Intel;
+using PangeaCyber.Net.Exceptions;
+using System.Text;
+using System.Security.Cryptography;
+
+
+class Program
+{
+
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            // Load client config. Can create it manually with Config constructor and setters
+            var clientCfg = Config.FromEnvironment("user-intel");
+
+            UserIntelClient client = new UserIntelClient.Builder(clientCfg).Build();
+
+            // Set the password you would like to check
+            string password = "mypassword";
+
+            // Calculate its hash, it could be sha256 or sha1
+            string hash = Utils.GetSHA256Hash(password);
+
+            // get the hash prefix, right now it should be just 5 characters
+            string hashPrefix = Utils.GetHashPrefix(hash, 5);
+
+            Response<UserPasswordBreachedResult>? response = null;
+
+            try
+            {
+                response = await client.Breached(new UserPasswordBreachedRequest.Builder(HashType.SHA256, hashPrefix)
+                    .WithVerbose(true)
+                    .WithRaw(true)
+                    .Build());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Fail to perform request: " + e);
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Request success");
+            Console.WriteLine("User password found in breach?: " + response.Result.Data.FoundInBreach);
+            Console.WriteLine($"Found in {response.Result.Data.BreachCount} breachs");
+
+            // This auxiliary function analyze service provider raw data to search for full hash in their registers
+            PasswordStatus status = UserIntelClient.IsPasswordBreached(response.Result, hash);
+            if (status == PasswordStatus.Breached)
+            {
+                Console.WriteLine($"Password '{password}' has been breached");
+            }
+            else if (status == PasswordStatus.Unbreached)
+            {
+                Console.WriteLine($"Password '{password}' has not been breached");
+            }
+            else if (status == PasswordStatus.Inconclusive)
+            {
+                Console.WriteLine($"Not enough information to confirm if password '{password}' has been or has not been breached.");
+            }
+            else
+            {
+                Console.WriteLine($"Unknown status: {status}");
+            }
+
+        }
+        catch (PangeaAPIException e)
+        {
+            Console.WriteLine("Failed with exception: " + e.ToString());
+        }
+    }
+}
--- packages/pangea-sdk/PangeaCyber.Net/Utils/Convertions.cs
+++ packages/pangea-sdk/PangeaCyber.Net/Utils/Convertions.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Security.Cryptography;
 
 namespace PangeaCyber.Net
 {
@@ -15,5 +16,42 @@ namespace PangeaCyber.Net
             // Convert the byte array to a Base64 string
             return Convert.ToBase64String(bytes);
         }
+
+        ///
+        public static string Base64ToString(string input){
+            // Convert Base64 string to byte array
+            byte[] bytes = Convert.FromBase64String(input);
+
+            // Decode byte array to string
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        ///
+        public static string GetSHA256Hash(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in hashBytes)
+                {
+                    builder.Append(b.ToString("x2")); // Convert each byte to its hexadecimal representation
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        ///
+        public static string GetHashPrefix(string input, int len = 5){
+            if(string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            return input.Substring(0, len);
+        }
+
     }
 }
