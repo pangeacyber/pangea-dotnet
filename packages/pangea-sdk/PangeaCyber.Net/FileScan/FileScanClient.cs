58339d6a5c01facae45ebbaa5b29d1f760d46777
David Wayman <david.wayman@pangea.cloud>
2023-09-08 15:31:33 -0700

    Progress on file scan docs

--- packages/pangea-sdk/PangeaCyber.Net/FileScan/FileScanClient.cs
+++ packages/pangea-sdk/PangeaCyber.Net/FileScan/FileScanClient.cs
@@ -1,6 +1,9 @@
 namespace PangeaCyber.Net.FileScan
 {
-    ///
+    /// <kind>class</kind>
+    /// <summary>
+    /// FileScan Client
+    /// </summary>
     public class FileScanClient : BaseClient<FileScanClient.Builder>
     {
         ///
@@ -27,7 +30,23 @@ namespace PangeaCyber.Net.FileScan
             }
         }
 
-        ///
+        /// <kind>method</kind>
+        /// <summary>Retrieve a reputation score for an IP address from a provider, including an optional detailed report.</summary>
+        /// <remarks>Reputation</remarks>
+        /// <operationid>ip_intel_post_v1_reputation</operationid>
+        /// <param name="request" type="PangeaCyber.Net.Intel.IPReputationRequest">IPReputationRequest with the ip to be looked up</param>
+        /// <returns>Response&lt;IPReputationResult&gt;</returns>
+        /// <example>
+        /// <code>
+        /// var request = new IPReputationRequest.Builder(
+        ///     "93.231.182.110")
+        ///     .WithProvider("crowdstrike")
+        ///     .WithVerbose(true)
+        ///     .WithRaw(true)
+        ///     .Build();
+        /// var response = await client.Reputation(request);
+        /// </code>
+        /// </example>
         public async Task<Response<FileScanResult>> Scan(FileScanRequest request, FileStream file)
         {
             return await DoPost<FileScanResult>("/v1/scan", request, file);
