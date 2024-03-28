0b0baa3e7894dd703687de850f266af0bb4b1fb2
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-03-28 15:15:51 -0400

    Add Sanitize examples (GEA-13537)

---
+++ examples/Sanitize/Share/Program.cs
@@ -0,0 +1,24 @@
+using PangeaCyber.Net;
+using PangeaCyber.Net.Sanitize;
+
+var cfg = Config.FromEnvironment("sanitize");
+var client = new SanitizeClient.Builder(cfg).Build();
+
+// Pick a file to sanitize.
+var file = new FileStream("./sample.pdf", FileMode.Open, FileAccess.Read);
+
+// Sanitize the file.
+var response = await client.Sanitize(
+    new SanitizeRequest()
+    {
+        RequestTransferMethod = TransferMethod.PostURL,
+        ShareOutput = new ShareOutput
+        {
+            Enabled = true,
+            OutputFolder = "/sdk_examples/sanitize/"
+        },
+        UploadedFileName = "sample.pdf",
+    },
+    file
+);
+Console.WriteLine($"Sanitized file is available in Secure Share with ID '{response.Result.DestShareID}'.");
