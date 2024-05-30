07cdeb2dedb866d67859eb94b406aa352a564083
H. Andrés Tournour <111303288+pangea-andrest@users.noreply.github.com>
2024-05-30 15:52:45 -0300

    Update packages/pangea-sdk/PangeaCyber.Net/Audit/Requests/ResultsRequest.cs
    Co-authored-by: Kenan Yildirim <kenan.yildirim@pangea.cloud>

--- packages/pangea-sdk/PangeaCyber.Net/Audit/Requests/ResultsRequest.cs
+++ packages/pangea-sdk/PangeaCyber.Net/Audit/Requests/ResultsRequest.cs
@@ -62,7 +62,7 @@ namespace PangeaCyber.Net.Audit
             }
 
             ///
-            public Builder WithReturnContext(bool? rc)
+            public Builder WithReturnContext(bool rc)
             {
                 ReturnContext = rc;
                 return this;
