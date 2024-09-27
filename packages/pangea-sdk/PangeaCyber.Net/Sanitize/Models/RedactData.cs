56c209efcfd250e6752d2c8d7bf19cb36c4404be
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-09-27 15:22:27 -0400

    Add detect-only Redact for Sanitize (PAN-17058)

--- packages/pangea-sdk/PangeaCyber.Net/Sanitize/Models/RedactData.cs
+++ packages/pangea-sdk/PangeaCyber.Net/Sanitize/Models/RedactData.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using PangeaCyber.Net.Sanitize.Models;
 
 namespace PangeaCyber.Net.Sanitize
 {
@@ -7,11 +8,15 @@ namespace PangeaCyber.Net.Sanitize
     {
         /// <summary>Number of items redacted.</summary>
         [JsonProperty("redaction_count")]
-        public int? RedactionCount { get; set; }
+        public int RedactionCount { get; private set; } = default!;
 
         /// <summary>Summary counts.</summary>
         [JsonProperty("summary_counts")]
-        public Dictionary<string, object>? SummaryCounts { get; set; }
+        public Dictionary<string, int> SummaryCounts { get; private set; } = default!;
+
+        /// <summary>The scoring result of a set of rules.</summary>
+        [JsonProperty("recognizer_results")]
+        public List<RedactRecognizerResult> RecognizerResults { get; private set; } = default!;
 
         /// <summary>Constructor.</summary>
         public RedactData() { }
