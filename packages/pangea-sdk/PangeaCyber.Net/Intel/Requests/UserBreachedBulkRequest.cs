5b0b53e7e09593912197fd984d515d6c019f949d
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-10-04 11:27:10 -0300

    Add support for `domains` field in `v2/user/breached` endpoint in User Intel service.

--- packages/pangea-sdk/PangeaCyber.Net/Intel/Requests/UserBreachedBulkRequest.cs
+++ packages/pangea-sdk/PangeaCyber.Net/Intel/Requests/UserBreachedBulkRequest.cs
@@ -18,6 +18,10 @@ namespace PangeaCyber.Net.Intel
         public string[]? Ips { get; set; }
 
         ///
+        [JsonProperty("domains")]
+        public string[]? Domains { get; set; }
+
+        ///
         [JsonProperty("phone_numbers")]
         public string[]? PhoneNumbers { get; set; }
 
@@ -35,6 +39,7 @@ namespace PangeaCyber.Net.Intel
             Emails = builder.Emails;
             Usernames = builder.Usernames;
             Ips = builder.Ips;
+            Domains = builder.Domains;
             PhoneNumbers = builder.PhoneNumbers;
             Start = builder.Start;
             End = builder.End;
@@ -49,6 +54,8 @@ namespace PangeaCyber.Net.Intel
             public string[]? Usernames { get; set; }
             ///
             public string[]? Ips { get; set; }
+            ///
+            public string[]? Domains { get; set; }
             ///
             public string[]? PhoneNumbers { get; set; }
             ///
@@ -80,6 +87,13 @@ namespace PangeaCyber.Net.Intel
             public Builder WithIPs(string[] ips)
             {
                 Ips = ips;
+                return this;
+            }
+
+            ///
+            public Builder WithDomains(string[] domains)
+            {
+                Domains = domains;
                 return this;
             }
 
