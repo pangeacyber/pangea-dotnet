5526fe6dd164578ef7be37edfda09e5c0f298c57
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-10-15 11:08:04 -0400

    Publish v3.13.0

--- packages/pangea-sdk/PangeaCyber.Net/Config.cs
+++ packages/pangea-sdk/PangeaCyber.Net/Config.cs
@@ -14,7 +14,7 @@ namespace PangeaCyber.Net
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "3.12.0";
+        public const string Version = "3.13.0";
 
         #endregion Constants
 
