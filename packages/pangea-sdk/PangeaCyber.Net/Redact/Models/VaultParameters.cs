d41e5fb012e426dbd60fd293a3f3b00060989864
H. Andrés Tournour <111303288+pangea-andrest@users.noreply.github.com>
2024-12-10 13:29:11 -0300

    Add `vault_parameters` and `llm_request` fields support on Redact ser… (#498)
    * Add `vault_parameters` and `llm_request` fields support on Redact service.
    
    * fixup! typos

---
+++ packages/pangea-sdk/PangeaCyber.Net/Redact/Models/VaultParameters.cs
@@ -0,0 +1,23 @@
+using Newtonsoft.Json;
+
+namespace PangeaCyber.Net.Redact
+{
+    /// <kind>class</kind>
+    /// <summary>
+    /// VaultParameters
+    /// </summary>
+    public class VaultParameters
+    {
+        /// <summary>
+        /// A vault key ID of an exportable key used to redact with FPE instead of using the service config default.
+        /// </summary>
+        [JsonProperty("fpe_key_id")]
+        public string? FPEkeyID { get; set; } = default!;
+
+        /// <summary>
+        /// A vault secret ID of a secret used to salt a hash instead of using the service config default.
+        /// </summary>
+        [JsonProperty("salt_secret_id")]
+        public string? SaltSecretID { get; set; } = default!;
+    }
+}
