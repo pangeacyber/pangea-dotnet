a31076e720154475b5ec278b8e261c3732776522
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-05-09 13:59:44 -0400

    Correct `RedactionMethodOverrides.RedactionType` JSON property name

--- packages/pangea-sdk/PangeaCyber.Net/Redact/Models/RedactionMethodOverrides.cs
+++ packages/pangea-sdk/PangeaCyber.Net/Redact/Models/RedactionMethodOverrides.cs
@@ -1,28 +1,27 @@
 using Newtonsoft.Json;
 
-namespace PangeaCyber.Net.Redact
+namespace PangeaCyber.Net.Redact;
+
+/// <summary>This field allows users to specify the redaction method per rule and its various parameters.</summary>
+public sealed class RedactionMethodOverrides
 {
-    /// <summary>This field allows users to specify the redaction method per rule and its various parameters.</summary>
-    public class RedactionMethodOverrides
-    {
-        ///
-        [JsonProperty("masking_type")]
-        public RedactType RedactionType { get; set; } = default!;
+    ///
+    [JsonProperty("redaction_type")]
+    public RedactType RedactionType { get; set; } = default!;
 
-        ///
-        [JsonProperty("hash")]
-        public Dictionary<string, object>? Hash { get; set; } = default!;
+    ///
+    [JsonProperty("hash")]
+    public IReadOnlyDictionary<string, object>? Hash { get; set; } = default!;
 
-        ///
-        [JsonProperty("fpe_alphabet")]
-        public FPEAlphabet? FpeAlphabet { get; set; } = default!;
+    ///
+    [JsonProperty("fpe_alphabet")]
+    public FPEAlphabet? FpeAlphabet { get; set; } = default!;
 
-        ///
-        [JsonProperty("partial_masking")]
-        public PartialMasking? PartialMasking { get; set; } = default!;
+    ///
+    [JsonProperty("partial_masking")]
+    public PartialMasking? PartialMasking { get; set; } = default!;
 
-        ///
-        [JsonProperty("redaction_value")]
-        public string? RedactionValue { get; set; } = default!;
-    }
+    ///
+    [JsonProperty("redaction_value")]
+    public string? RedactionValue { get; set; } = default!;
 }
