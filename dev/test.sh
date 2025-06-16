78974589767523af7d88ecb1d53068505d6bdc60
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-06-16 10:45:35 -0400

    Update AI Guard OpenAPI spec

--- dev/test.sh
+++ dev/test.sh
@@ -5,6 +5,6 @@ set -e
 cd -- "$(dirname -- "$0")/.."
 
 pnpm dlx start-server-and-test --expect 404 \
-  "pnpm dlx @stoplight/prism-cli mock -d --json-schema-faker-fillProperties=false dev/specs/ai-guard_openapi.json" \
+  "pnpm dlx @stoplight/prism-cli mock -d --json-schema-faker-fillProperties=false dev/specs/ai-guard.openapi.json" \
   4010 \
   "dotnet test packages/pangea-sdk/PangeaCyber.Tests --filter AIGuardClientTests"
